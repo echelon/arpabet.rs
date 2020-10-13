b8a4483d1c2f4b200d51ebf972ad8d3e89c3e17c
Brandon Thomas <echelon@gmail.com>
2020-10-13 04:26:31 -0400

    cached Arpabet w/ cmudict, more tests

--- arpabet_cmudict/src/lib.rs
+++ arpabet_cmudict/src/lib.rs
@@ -16,6 +16,9 @@
 //! includes this transitively.
 //! ```
 
+#[cfg(test)] #[macro_use] extern crate expectest;
+#[cfg(test)] extern crate chrono;
+#[macro_use] extern crate lazy_static;
 extern crate arpabet_types;
 extern crate phf;
 
@@ -23,15 +26,31 @@ use arpabet_types::Arpabet;
 
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
-/// Load the CMUdict.
-pub fn load_cmudict() -> Arpabet {
+lazy_static! {
+  // TODO: When static constexpr are added to Rust, evaluate this at compile time.
+  // Lazily cached copy of the entire CMU arpabet.
+  static ref ARPABET_CMU_DICT : Arpabet = load_cmudict_lazy();
+}
+
+/// Load the in-memory CMUdict.
+/// The first call lazily converts an already in-memory phf::Map into wrapped HashMap.
+/// In the future this may be further optimized away.
+pub fn load_cmudict() -> &'static Arpabet {
+  &ARPABET_CMU_DICT
+}
+
+/// Load the CMUdict lazily and cache it.
+fn load_cmudict_lazy() -> Arpabet {
   Arpabet::from_phf_map(&CMU_DICT)
 }
 
 #[cfg(test)]
 mod tests {
+  use chrono::prelude::*;
+  use expectest::prelude::*;
+
   // NB: Codegen.
-  use super::CMU_DICT;
+  use super::{CMU_DICT, load_cmudict};
   use arpabet_types::{Phoneme, Consonant, Vowel, VowelStress};
 
   #[test]
@@ -41,7 +60,6 @@ mod tests {
 
   #[test]
   fn test_cmudict_codegen_entries() {
-    // Y OW1 SH IY0
     let expected = vec![
       Phoneme::Consonant(Consonant::Y),
       Phoneme::Vowel(Vowel::OW(VowelStress::PrimaryStress)),
@@ -49,5 +67,42 @@ mod tests {
       Phoneme::Vowel(Vowel::IY(VowelStress::NoStress)),
     ];
     assert_eq!(CMU_DICT.get("yoshi").map(|res| res.to_vec()), Some(expected));
+  }
+
+  #[test]
+  fn test_load_cmudict() {
+    let arpabet = load_cmudict();
+
+    assert_eq!(arpabet.get_polyphone_str("game"),
+      Some(vec!["G", "EY1", "M"]));
+
+    assert_eq!(arpabet.get_polyphone_str("boy"),
+      Some(vec!["B", "OY1"]));
+
+    assert_eq!(arpabet.get_polyphone_str("advance"),
+      Some(vec!["AH0", "D", "V", "AE1", "N", "S"]));
+
+    assert_eq!(arpabet.get_polyphone_str("sp"), None);
+
+    assert_eq!(arpabet.get_polyphone_str("ZZZZZ"), None);
+  }
+
+  #[test]
+  fn test_cmudict_is_cached() {
+    let _ = load_cmudict(); // pre-cache
+
+    let start = Utc::now();
+
+    for _ in 0 .. 1000 {
+      // This should be cached...
+      let arpabet = load_cmudict();
+
+      assert_eq!(arpabet.get_polyphone_str("yep"),
+        Some(vec!["Y", "EH1", "P"]));
+    }
+
+    let end = Utc::now();
+    let duration = end.signed_duration_since(start);
+    expect!(duration.num_milliseconds()).to(be_less_than(1_000));
   }
 }
