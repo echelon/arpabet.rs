1162f74eb33e8f31700031874c577fed3245cfa1
Brandon Thomas <echelon@gmail.com>
2020-10-13 01:19:30 -0400

    cleanup codegen

--- arpabet_cmudict/build.rs
+++ arpabet_cmudict/build.rs
@@ -1,40 +1,36 @@
+// Copyright (c) 2020 Brandon Thomas <bt@brand.io>, <echelon@gmail.com>
 #[macro_use] extern crate lazy_static;
 
-extern crate arpabet_types;
 extern crate arpabet_parser;
+extern crate arpabet_types;
 extern crate phf_codegen;
 
+use arpabet_types::{Arpabet, Consonant, Phoneme, Polyphone, VowelStress};
+use phf_codegen::Map;
 use std::env;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::Path;
-use phf_codegen::Map;
-use arpabet_types::{Polyphone, VowelStress};
-use arpabet_types::Consonant;
-use arpabet_types::Arpabet;
-use arpabet_types::Phoneme;
-
-//pub type Word = String;
-//pub type Polyphone = Vec<Phoneme>;
 
 const CMU_DICT_TEXT : &'static str = include_str!("../cmudict/cmudict-0.7b");
 
 lazy_static! {
+  // NB: Static lifetime is required for use in phf_codegen.
   static ref CMUDICT : Arpabet = arpabet_parser::load_from_str(CMU_DICT_TEXT)
       .expect("Must parse at compile time");
 }
 
 fn main() {
   let path = Path::new(&env::var("OUT_DIR").unwrap()).join("codegen.rs");
+
   let mut file = BufWriter::new(File::create(&path).unwrap());
 
   write!(&mut file, "use arpabet_types::Consonant;\n").unwrap();
   write!(&mut file, "use arpabet_types::Vowel;\n").unwrap();
   write!(&mut file, "use arpabet_types::VowelStress;\n").unwrap();
-  //write!(&mut file, "use arpabet_types::Polyphone;\n").unwrap();
 
-  //write!(&mut file, "static KEYWORDS: phf::Map<&'static str, Keyword> = ").unwrap();
-  write!(&mut file, "static CMU_DICT_2: phf::Map<&'static str, &'static [Phoneme]> = ").unwrap();
+  write!(&mut file, "/// Compile-time CMUdict via codegen (see 'arpabet_cmudict/build.rs')").unwrap();
+  write!(&mut file, "pub static CMU_DICT_2: phf::Map<&'static str, &'static [Phoneme]> = ").unwrap();
 
   let mut builder : Map<&'static str> = phf_codegen::Map::new();
 
@@ -62,23 +58,11 @@ fn main() {
 
     code.push_str("]");
 
-    //builder.entry(&key, "&[ Phoneme::Consonant(Consonant::B) ]");
     builder.entry(&key, &code);
-    println!("Key: {}", key);
-
   }
 
-  //let codegen_2 = builder
-   // .entry("TEST", "&[ Phoneme::Consonant(Consonant::B) ]");
-  //.entry("TEST", "unsafe { vec![] }");
-  //.entry("continue", "Keyword::Continue")
-
-  let codegen_map = builder
-    .build();
-    //.build(&mut file)
-    //.unwrap();
+  let codegen_map = builder.build();
 
   write!(&mut file, "{}", codegen_map.to_string()).unwrap();
-
   write!(&mut file, ";\n").unwrap();
 }
